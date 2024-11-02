c8a2edf75a72f1d507a5b2ca65ef4841dcbc6dcd
djallel <bouziane.djalleleddine@gmail.com>
2024-11-02 11:37:23 +0100

    2024

--- src/components/Phone.tsx
+++ src/components/Phone.tsx
@@ -1,21 +1,22 @@
-import { cn } from '@/lib/utils'
-import Image from 'next/image'
-import React, { HtmlHTMLAttributes } from 'react'
+import { cn } from '@/lib/utils';
+import { HTMLAttributes } from 'react';
 
-interface PhoneProps extends HtmlHTMLAttributes<HTMLDivElement> {
-  imgSrc: string
-  dark?: boolean
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
 }
 
-const Phone = ({imgSrc, className, dark=false, ...props} : PhoneProps) => {
+const Phone = ({ imgSrc, className, dark = false, ...props }: PhoneProps) => {
   return (
     <div
       className={cn(
         'relative pointer-events-none z-50 overflow-hidden',
         className
       )}
-      {...props}>
-      <Image
+      {...props}
+    >
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src={
           dark
             ? '/phone-template-dark-edges.png'
@@ -23,20 +24,18 @@ const Phone = ({imgSrc, className, dark=false, ...props} : PhoneProps) => {
         }
         className='pointer-events-none z-50 select-none'
         alt='phone image'
-        width={100}
-        height={100}
       />
 
       <div className='absolute -z-10 inset-0'>
-        <Image
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
           className='object-cover min-w-full min-h-full'
           src={imgSrc}
           alt='overlaying phone image'
-          layout="fill"
         />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Phone
+export default Phone;
