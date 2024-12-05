0f1cc890ac2eb3432efeb223b095a29ebf727208
djallel <bouziane.djalleleddine@gmail.com>
2024-12-05 11:33:00 +0100

    05/12

--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,12 +1,18 @@
 "use client"
 
 import { cn } from "@/lib/utils"
-import { useState } from "react"
+import { Image, Loader2, MousePointerSquareDashed } from "lucide-react"
+import { useState, useTransition } from "react"
 import Dropzone, { FileRejection } from "react-dropzone"
+import {Progress} from "@nextui-org/progress";
 
 const page = () => {
 
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const [isDragOver , setIsDragOver] = useState<boolean>(false)
+
+  const isUploading = false;
+  const [isPending , startTransition ] = useTransition() 
 
   const onDropRejected = () => {}
   const onDropAccepted = () => {}
@@ -30,6 +36,37 @@ const page = () => {
           {({getRootProps, getInputProps}) => (
            <div className="h-full w-full flex-1 flex flex-col items-center justify-center" {...getRootProps()}>
               <input {...getInputProps()}/>
+              {isDragOver ? (
+                <MousePointerSquareDashed className="h-6 w-6  text-zinc-500 mb-2"/> 
+              ) : isUploading ? ( 
+              <Loader2 className="animate-spin h-6 w-6 text-zinc-500 mb-2" /> 
+              ) : (
+              <Image className="h-6 w-6 mb-2 text-zinc-500"/>
+            )}
+            <div className="flex flex-col justify-center mb-2 text-sm text-zinc-700">
+              {isUploading ? (
+                    <div className='flex flex-col items-center'>
+                      <p>Uploading...</p>
+                      <Progress
+                        value={uploadProgress}
+                        className='mt-2 w-40 h-2 bg-gray-300'
+                      />
+                    </div>
+                  ) : isPending ? (
+                    <div className='flex flex-col items-center'>
+                      <p>Redirecting, please wait...</p>
+                    </div>
+                  ) : isDragOver ? (
+                    <p>
+                      <span className='font-semibold'>Drop file</span> to upload
+                    </p>
+                  ) : (
+                    <p>
+                      <span className='font-semibold'>Click to upload</span> or
+                      drag and drop
+                    </p>
+              )}
+            </div>
            </div>
           )}
         </Dropzone>
@@ -39,3 +76,6 @@ const page = () => {
 }
 
 export default page
+
+
+// 2.42
